adc9a246687a4ccbe3a11ed230eda420db94559d
JaeminRyu / 류재민 <83535846+ryujm1828@users.noreply.github.com>
2022-01-19 17:42:33 +0900

    Create 11727.c

---
+++ BOJ/11727/11727.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main(){
+	int k[1005];
+	k[1] = 1;
+	k[2] = 3;
+	for(int i=3;i<1001;i++){
+		k[i] = (k[i-1] + k[i-2] + k[i-2])%10007;
+	}
+	int n;
+	scanf("%d",&n);
+	printf("%d",k[n]);
+}
