7de246d6bb48f4c1f1ce6e0e49aa0d617b07b3fa
JaeminRyu / 류재민 <83535846+ryujm1828@users.noreply.github.com>
2022-01-22 09:27:22 +0900

    Create 15990_1.c

---
+++ BOJ/15990/15990_1.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+long one[150000];
+long two[150000];
+long thr[150000];
+
+int main(){
+
+	one[1] = 1;
+	two[1] = 0;
+	thr[1] = 0;
+    
+	one[2] = 0;
+	two[2] = 1;
+	thr[2] = 0;
+    
+	one[3] = 1;
+	two[3] = 1;
+	thr[3] = 1;
+	for(int i=4; i<=100000; i++){
+		one[i] = (thr[i-1] + two[i-1])%1000000009;
+		two[i] = (one[i-2] + thr[i-2])%1000000009;
+		thr[i] = (one[i-3] + two[i-3])%1000000009;
+	}
+	int t;
+	int n;
+	scanf("%d",&t);
+	while(t--){
+		scanf("%d",&n);
+		printf("%ld\n",(one[n]+two[n]+thr[n])%1000000009);
+	}
+}
