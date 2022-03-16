231b44add7fd3f8684674fd09b35c99fadcd69c4
JaeminRyu / 류재민 <ryujm1828@gmail.com>
2022-03-16 20:21:48 +0900

    Create 9656.cpp

---
+++ BOJ/9656/9656.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+bool arr[1001];
+
+int main(){
+	arr[1] = 0;
+	arr[2] = 1;
+	arr[3] = 0;
+	arr[4] = 1;
+	
+	for(int i=5; i<1001; i++)
+		arr[i] = arr[i-4];
+		
+	int n;
+	scanf("%d",&n);
+	if(arr[n] == 0)
+		printf("SK");
+	else
+		printf("CY");
+	
+}
